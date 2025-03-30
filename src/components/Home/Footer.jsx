a7a6e8fef214ee72b2d48b38da4681bafcadf965
Kapitour <plataformadigitalkapitour@gmail.com>
2025-03-30 11:35:17 -0300

    Update Footer.jsx

--- src/components/Home/Footer.jsx
+++ src/components/Home/Footer.jsx
@@ -5,26 +5,27 @@ const FooterContainer = styled.footer`
   padding: 20px;
   background-color: #333333c8;
   text-align: center;
-  margin-top: 220px;
+  margin-top: 50px;
   backdrop-filter: blur(7px);
+  width: 100%;
+  position: relative;
 
-  @media (min-width: 320px) {
-    height: 43px;
-  } 
+  @media (max-width: 768px) {
+    margin-top: 30px;
+    padding: 15px;
+  }
 `;
 
 // Estilo para o texto do copyright
 const Copyright = styled.div`
   text-align: center;
-  margin-top: -10px;
   font-size: 14px;
-  color:rgb(255, 255, 255);
+  color: rgb(255, 255, 255);
 `;
 
 const Footer = () => {
   return (
     <FooterContainer>
-
       <Copyright>
         <p>&copy; 2024 Kapitour. Todos os direitos reservados.</p>
       </Copyright>
